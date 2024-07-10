8ec75c677f16b27cff722d825719b2d0089e9f9e
Hakob <hkeshis911@student.glendale.edu>
2024-07-09 23:25:03 -0700

    Update Amazon_Product_Search.py
    No Longer Need to Download Chrome Web Driver and Locate Its Path

--- Automated-Web-Browsing/Amazon_Product_Search.py
+++ Automated-Web-Browsing/Amazon_Product_Search.py
@@ -7,9 +7,7 @@ from selenium.webdriver.common.by import By
 
 
 #Initial Setup
-#Change this file path to the correct file path of your google chrom web driver
-path = "C:\Program Files (x86)\chromedriver.exe"
-driver = webdriver.Chrome(path)
+driver = webdriver.Chrome()
 driver.get('https://www.amazon.com/?tag=amazusnavi-20&hvadid=381823327672&hvpos=&hvnetw=g&hvrand=15369903253882284441&hvpone=&hvptwo=&hvqmt=e&hvdev=c&hvdvcmdl=&hvlocint=&hvlocphy=9031162&hvtargid=kwd-10573980&ref=pd_sl_7j18redljs_e&hydadcr=28883_11845445&gclid=CjwKCAjwmJeYBhAwEiwAXlg0ATpf1Z-JL2kpyEl7yRZRhGWjvKzG3tVkde1CAQCiNZ9JaYRHyJXjjRoCf5wQAvD_BwE')
 
 
