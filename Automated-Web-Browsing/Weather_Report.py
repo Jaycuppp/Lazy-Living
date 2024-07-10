320275a556677b3461c5bb14b011546264ff73e5
Hakob <hkeshis911@student.glendale.edu>
2024-07-09 23:25:13 -0700

    Update Weather_Report.py

--- Automated-Web-Browsing/Weather_Report.py
+++ Automated-Web-Browsing/Weather_Report.py
@@ -5,8 +5,7 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 
 #Initial Setup
-path = "C:\Program Files (x86)\chromedriver.exe"
-driver = webdriver.Chrome(path)
+driver = webdriver.Chrome()
 driver.get('https://weather.com/weather/today/l/c3c69cfc9248e306c9c54533c6b25eca5c36122f5ebebc50c48908877ccfcb97')
 
 
